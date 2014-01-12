1fed2640e5045320ade402845eff7034aaccd1b4
Z. Bornheimer <z@chary.me>
2014-01-12 09:23:29 -0500

    Add back versions and run closure on all scripts
    If a script's handle is defined in the ignore array, it will use closure.cgi, but otherwise it will load async.
    
    Incremented version number as well.

--- zyscripts.php
+++ zyscripts.php
@@ -3,7 +3,7 @@
  * Plugin Name: Zyscripts Enhancements
  * Plugin URI: http://zysys.org/wiki/Zyscript_Enhancements
  * Description: A series of site enhancements that optimize the site beyond caching
- * Version: 1.4
+ * Version: 1.5
  * Author: Z. Bornheimer (Zysys)
  * Author URI: http://zysys.org
  * License: GPLv3
@@ -15,18 +15,13 @@ function zyscripts_loadjs($url) {
     $ignoreArray = array('jquery', 'jetpack', 'superfish', 'wpgroho', 'flexslider', 'flowplayer');
     foreach ($ignoreArray as $ignore) { 
         if (strpos($url, $ignore) !== false) {
-            return $url;
+            return str_replace($url, '//zyscripts.com/closure.cgi/'.urlencode(urlencode(urlencode($url))), $url);
         }
     }
     return str_replace($url, '//zyscripts.com/loadjs.cgi/'.urlencode(urlencode(urlencode($url))), $url);
 }
 add_filter( 'script_loader_src', 'zyscripts_loadjs' );
 
-function zyscripts_loadjs_rm_ver( $src, $handle ) {
-    $src = remove_query_arg( 'ver', $src );
-    return $src;
-}
-add_filter( 'script_loader_src', 'zyscripts_loadjs_rm_ver', 10, 2 );
 endif;
 
 if (!function_exists('zyscripts_loadcss')):
@@ -35,12 +30,6 @@ function zyscripts_loadcss($url) {
     return str_replace($url, '//zyscripts.com/css.cgi/'.urlencode(urlencode(urlencode($url))), $url);
 }
 add_filter( 'style_loader_src', 'zyscripts_loadcss' );
-
-function zyscripts_loadcss_rm_ver( $src, $handle ) {
-    $src = remove_query_arg( 'ver', $src );
-    return $src;
-}
-add_filter( 'style_loader_src', 'zyscripts_loadcss_rm_ver', 10, 2 );
 endif;
 
 if (!function_exists('zyscripts_updater_init')):
